8202f81ee01625c2dceae06249a9c1bde7598733
chacha86 <taejincha0619@gmail.com>
2024-05-09 00:32:32 +0900

    ms4 complete, todo refactoring

--- src/main/java/com/example/ms1/auth/oauth2/MyOauth2UserService.java
+++ src/main/java/com/example/ms1/auth/oauth2/MyOauth2UserService.java
@@ -11,6 +11,7 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -20,29 +21,73 @@ public class MyOauth2UserService extends DefaultOAuth2UserService {
 
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
-        String registrationId = userRequest.getClientRegistration().getRegistrationId();
         OAuth2User user = super.loadUser(userRequest);
+        String registrationId = userRequest.getClientRegistration().getRegistrationId();
 
+        switch (registrationId) {
+            case "google":
+                return googleService(user);
+            case "kakao":
+                return kakaoService(user);
+            case "naver":
+                return naverService(user);
+        }
+
+        return super.loadUser(userRequest);
+    }
+
+    private OAuth2User googleService(OAuth2User user) {
+
+        String registrationId = "google";
         String sub = user.getAttribute("sub");
         String email = user.getAttribute("email");
-
-        System.out.println("registrationId = " + registrationId);
-        System.out.println("sub = " + sub);
-        System.out.println("email = " + email);
 
         Member member = memberRepository.findByLoginId(sub).orElse(null);
 
         if(member == null) {
-            member = new Member();
-            member.setLoginId(sub);
-            member.setEmail(email);
-            member.setNickname(registrationId + "_" + sub);
-            member.setPassword("");
-            member.setCreateDate(LocalDateTime.now());
-
-            memberRepository.save(member);
+            saveMember(registrationId, sub, email);
         }
 
-        return super.loadUser(userRequest);
+        return user;
+    }
+
+    private OAuth2User kakaoService(OAuth2User user) {
+        String registrationId = "kakao";
+        Long sub = user.getAttribute("id");
+        String email = null;
+
+        Member member = memberRepository.findByLoginId(String.valueOf(sub)).orElse(null);
+
+        if(member == null) {
+            saveMember(registrationId, String.valueOf(sub), email);
+        }
+
+        return user;
+    }
+
+    private OAuth2User naverService(OAuth2User user) {
+        String registrationId = "naver";
+        Map<String, String> response = user.getAttribute("response");
+        String sub = response.get("id");
+        String email = response.get("email");
+
+        Member member = memberRepository.findByLoginId(sub).orElse(null);
+
+        if(member == null) {
+            saveMember(registrationId, sub, email);
+        }
+
+        return user;
+    }
+
+    private Member saveMember(String registrationId, String sub, String email) {
+        Member member = new Member();
+        member.setLoginId(sub);
+        member.setEmail(email);
+        member.setNickname(registrationId + "_" + sub);
+        member.setPassword("");
+        member.setCreateDate(LocalDateTime.now());
+
+        return memberRepository.save(member);
     }
 }
