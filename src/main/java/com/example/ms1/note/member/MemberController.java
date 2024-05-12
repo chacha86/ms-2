72dbe9c15fc34880a19e26256fda46cc36197250
chacha86 <taejincha0619@gmail.com>
2024-05-13 01:07:49 +0900

    paramhandler apply

--- src/main/java/com/example/ms1/note/member/MemberController.java
+++ src/main/java/com/example/ms1/note/member/MemberController.java
@@ -9,6 +9,7 @@ import lombok.Setter;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
