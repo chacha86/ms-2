f1c28a024c4a7c192f47c2050b78d02580510909
chacha86 <taejincha0619@gmail.com>
2024-05-16 01:17:20 +0900

    nextjs toast ui editor

---
+++ nextjs/src/app/myEditor.tsx
@@ -0,0 +1,22 @@
+'use client';
+import dynamic from "next/dynamic";
+import '@toast-ui/editor/dist/toastui-editor.css';
+import React, {useState, useEffect} from 'react';
+
+export default function MyEditor() {
+
+    const Editor = dynamic(
+        () => import('@toast-ui/react-editor').then(mod => mod.Editor),
+        { ssr: false }  // 이 옵션은 서버 사이드 렌더링을 비활성화합니다.
+    );
+
+    return (
+        <Editor
+            initialValue="hello react editor world!"
+            previewStyle="vertical"
+            height="600px"
+            initialEditType="markdown"
+            useCommandShortcut={true}
+        />
+    );
+}
---
+++ nextjs/src/app/test/page.tsx
@@ -0,0 +1,6 @@
+'use client';
+import MyEditor from '../myEditor';
+
+export default function MyComponent() {
+    return <MyEditor />;
+}
